f80f7d22dfff2352cc95b7d369389f47445a525f
Tomer Zait <realgam3@gmail.com>
2015-07-17 22:40:08 +0300

    install32.bat now works with x86 os

--- install32.bat
+++ install32.bat
@@ -4,12 +4,13 @@ mkdir %PLUGINDIR%
 copy bin\x32\x64dbg-python.dll %PLUGINDIR%\x64dbg_python.dp32
 
 cd swig
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
 set MSSDK=1
 set DISTUTILS_USE_SDK=1
 IF EXIST "%PROGRAMFILES(X86)%" (
+    call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
     for /f "tokens=3" %%x in ('reg query HKLM\SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath /ve') do set PYTHONDIR=%%x
 ) ELSE (
+    call "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
     for /f "tokens=3" %%x in ('reg query HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve') do set PYTHONDIR=%%x
 )
 "%PYTHONDIR%python.exe" setup.py  install --install-lib="..\%PLUGINDIR%"
--- install64.bat
+++ install64.bat
@@ -4,9 +4,9 @@ mkdir %PLUGINDIR%
 copy bin\x64\x64dbg-python.dll %PLUGINDIR%\x64dbg_python.dp64
 
 cd swig
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
 set MSSDK=1
 set DISTUTILS_USE_SDK=1
+call "%ProgramFiles(x86)%\Microsoft Visual Studio 9.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat"
 for /f "tokens=3" %%x in ('reg query HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve') do set PYTHONDIR=%%x
 "%PYTHONDIR%python.exe" setup.py  install --install-lib="..\%PLUGINDIR%"
 cd ..
