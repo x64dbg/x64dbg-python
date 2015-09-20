01030f67597a9e9b16f0c625dab6ac1f53f6819a
Storm Shadow <storm_shadow@techbliss.org>
2015-09-20 16:08:18 +0200

    added script editor to x+4 python change install32.bat to dl PyQt4 bundle

--- install32.bat
+++ install32.bat
@@ -3,8 +3,7 @@
 set PLUGINDIR=%~dp0\release\x32\plugins
 mkdir %PLUGINDIR%
 mkdir %PLUGINDIR%\X64Dbg_editor
-
-copy bin\x32\x64dbg-python.dll X64Dbg_editor\x64dbg_python.dp32
+echo F| XCOPY bin\x32\x64dbg-python.dll %PLUGINDIR%\x64dbg_python.dp32
 xcopy /e /c /y swig\build\lib.win32-2.7\x64dbg_python %PLUGINDIR%\x64dbg_python\
 copy swig\PyQt-win-gpl-4.11.4-x64dbg-edition-download.py %PLUGINDIR%\PyQt-win-gpl-4.11.4-x64dbg-edition-download.py   
 @cd swig
