4901ea886aa9b05c6aadffd96a25ec038b2ab4b0
Mr. eXoDia <mr.exodia.tpodt@gmail.com>
2016-07-01 11:28:26 +0200

    scripts now working on paths with spaces

--- install32.bat
+++ install32.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PLUGINDIR=%~dp0\release\x32\plugins
+set PLUGINDIR="%~dp0\release\x32\plugins"
 mkdir %PLUGINDIR%
 copy bin\x32\x64dbgpy.dll %PLUGINDIR%\x64dbgpy.dp32
 
@@ -8,7 +8,7 @@ copy bin\x32\x64dbgpy.dll %PLUGINDIR%\x64dbgpy.dp32
 call clean.bat
 call "%~dp0\setenv.bat"
 call %VCVARS% x86
-"%PYTHON27X86%\python.exe" setup.py  install --install-lib="%PLUGINDIR%"\x64dbgpy
+"%PYTHON27X86%\python.exe" setup.py  install --install-lib=%PLUGINDIR%\x64dbgpy
 @cd ..
 
 @echo on
--- install64.bat
+++ install64.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PLUGINDIR=%~dp0\release\x64\plugins
+set PLUGINDIR="%~dp0\release\x64\plugins"
 mkdir %PLUGINDIR%
 copy bin\x64\x64dbgpy.dll %PLUGINDIR%\x64dbgpy.dp64
 
@@ -8,7 +8,7 @@ copy bin\x64\x64dbgpy.dll %PLUGINDIR%\x64dbgpy.dp64
 call clean.bat
 call "%~dp0\setenv.bat"
 call %VCVARS% x86_amd64
-"%PYTHON27X64%\python.exe" setup.py  install --install-lib="%PLUGINDIR%"\x64dbgpy
+"%PYTHON27X64%\python.exe" setup.py  install --install-lib=%PLUGINDIR%\x64dbgpy
 @cd ..
 
 @echo on
