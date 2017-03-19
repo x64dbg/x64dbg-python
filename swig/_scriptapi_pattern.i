8b19d74ed513b7246621677836069247ba032af7
mrexodia <mr.exodia.tpodt@gmail.com>
2017-03-19 12:30:51 +0100

    fixed conflicting name in pattern.py

--- swig/_scriptapi_pattern.i
+++ swig/_scriptapi_pattern.i
@@ -5,4 +5,7 @@
 
 %pybuffer_mutable_binary(unsigned char* data, duint datasize)
 
+%rename(Pattern_Write) Script::Pattern::Write;
+%rename(Pattern_WriteMem) Script::Pattern::WriteMem;
+
 %include "..\pluginsdk\_scriptapi_pattern.h"
--- swig/x64dbgpy/pluginsdk/_scriptapi/pattern.py
+++ swig/x64dbgpy/pluginsdk/_scriptapi/pattern.py
@@ -7,11 +7,11 @@ def Find(data, pattern):
 def FindMem(start, size, pattern):
     return x64dbg.FindMem(start, size, pattern)
 
-def Write(data, pattern):
-    x64dbg.Write(data, pattern)
+def PatternWrite(data, pattern):
+    x64dbg.Pattern_Write(data, pattern)
 
-def WriteMem(start, size, pattern):
-    x64dbg.WriteMem(start, size, pattern)
+def PatternWriteMem(start, size, pattern):
+    x64dbg.Pattern_WriteMem(start, size, pattern)
 
 def SearchAndReplace(data, searchpattern, replacepattern):
     return x64dbg.SearchAndReplace(data, searchpattern, replacepattern)
